8c3a1b5859a865609d3b5d622888c4b68b7de40f
BobinaLiza <bobinaliza14@gmail.com>
2025-11-22 11:01:02 +0300

    init class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,48 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.system.exitProcess
+fun main() {
+    val pizzaPeter = PizzaPeter(
+        175.0, 241.5,
+        167.5, 215.0
+    )
+    val pizzaMoscow = PizzaMoscow(
+        215.0, 250.5,
+        180.5, 240.0
+    )
+    val currentPizzaCity:PizzaCity
+    println("Добрый день! Выберите город")
+    println("1. Москва\n2. Санкт-Петербург\n")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val city = readln()
+    if (city == "1") {
+        currentPizzaCity = pizzaMoscow
+        println("Выберите пиццу: ")
+        println("1. Неаполитанская пицца\n2. Римская пицца\n3. Сицилийская пицца\n4. Тирольская пицца")
+        when(readln()) {
+            "1" -> pizzaMoscow.neapolitanPizzaSale()
+            "2" -> pizzaMoscow.romanPizzaSale()
+            "3" -> pizzaMoscow.sicilianPizzaSale()
+            "4" -> pizzaMoscow.tyroleanPizzaSale()
+            else -> {
+                println("ERROR")
+                exitProcess(1)
+            }
+        }
+    } else if (city == "2") {
+        currentPizzaCity = pizzaPeter
+        println("Выберите пиццу: ")
+        println("1. Неаполитанская пицца\n2. Римская пицца\n3. Сицилийская пицца\n4. Тирольская пицца")
+        when(readln()) {
+            "1" -> pizzaMoscow.neapolitanPizzaSale()
+            "2" -> pizzaMoscow.romanPizzaSale()
+            "3" -> pizzaMoscow.sicilianPizzaSale()
+            "4" -> pizzaMoscow.tyroleanPizzaSale()
+            else -> {
+                println("ERROR")
+                exitProcess(1)
+            }
+        }
+    } else {
+        println("ERROR")
+        exitProcess(1)
+    }
 }
